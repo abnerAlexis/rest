f3fbc46e3a8c467957abb56e058a98bf62c5ade5
AJ A <alexiproj@gmail.com>
2021-12-22 01:21:46 -0700

    Update post request.

--- src/test/java/selenium/restassured/CreateUserPostRequest.java
+++ src/test/java/selenium/restassured/CreateUserPostRequest.java
@@ -3,8 +3,7 @@ package selenium.restassured;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 import java.util.HashMap;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.isA;
+import static org.hamcrest.Matchers.*;
 
 public class CreateUserPostRequest extends Base {
     private final static String PATH = URL + "api/users";
@@ -20,6 +19,82 @@ public class CreateUserPostRequest extends Base {
             .statusCode(HttpStatus.SC_CREATED)
             .body("name", is("morpheus"))
             .body("job", is("leader"))
+            .body("id", isA(String.class))
+            .body("createdAt", isA(String.class))
+        ;
+    }
+
+    @Test
+    public void createUser_EmptyValues() {
+        HashMap<String, String> body = new HashMap<>();
+        body.put("name", "");
+        body.put("job", "");
+
+        httpCallManager.postRequest(PATH, body)
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .body("name", is(""))
+            .body("job", is(""))
+            .body("id", isA(String.class))
+            .body("createdAt", isA(String.class))
+        ;
+    }
+
+    @Test
+    public void createUser_NullValues() {
+        HashMap<String, String> body = new HashMap<>();
+        body.put("name", null);
+        body.put("job", null);
+
+        httpCallManager.postRequest(PATH, body)
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .body("name", is(nullValue()))
+            .body("job", is(nullValue()))
+            .body("id", isA(String.class))
+            .body("createdAt", isA(String.class))
+        ;
+    }
+
+    @Test
+    public void createUser_NullName() {
+        HashMap<String, String> body = new HashMap<>();
+        body.put("name", null);
+        body.put("job", "job");
+
+        httpCallManager.postRequest(PATH, body)
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .body("name", is(nullValue()))
+            .body("job", is("job"))
+            .body("id", isA(String.class))
+            .body("createdAt", isA(String.class))
+        ;
+    }
+
+    @Test
+    public void createUser_NullJob() {
+        HashMap<String, String> body = new HashMap<>();
+        body.put("name", "name");
+        body.put("job", null);
+
+        httpCallManager.postRequest(PATH, body)
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .body("name", is("name"))
+            .body("job", is(nullValue()))
+            .body("id", isA(String.class))
+            .body("createdAt", isA(String.class))
+        ;
+    }
+
+    @Test
+    public void createUser_NoParams() {
+        HashMap<String, String> body = new HashMap<>();
+
+        httpCallManager.postRequest(PATH, body)
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
             .body("id", isA(String.class))
             .body("createdAt", isA(String.class))
         ;
